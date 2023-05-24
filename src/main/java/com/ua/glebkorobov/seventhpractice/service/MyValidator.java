a767244e317543ae3f51476ac64c5e8925b9dbd6
Gleb Korobov <gkorobov9@gmail.com>
2023-05-24 20:39:18 +0300

    added test for MyValidator.java

--- src/main/java/com/ua/glebkorobov/seventhpractice/service/MyValidator.java
+++ src/main/java/com/ua/glebkorobov/seventhpractice/service/MyValidator.java
@@ -8,6 +8,7 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.stream.LongStream;
 
 public class MyValidator implements ConstraintValidator<IpnValidator, Student> {
 
@@ -18,14 +19,14 @@ public class MyValidator implements ConstraintValidator<IpnValidator, Student> {
     @Override
     public boolean isValid(Student student, ConstraintValidatorContext context) {
         this.student = student;
-        return isIPNSizeCorrect() && isGenderCorrect() && isDateOfBirthday();
+        return isIPNSizeCorrect() && isGenderCorrect() && isDateOfBirthday() && isControlDigit();
     }
 
-    private boolean isIPNSizeCorrect() {
+    public boolean isIPNSizeCorrect() {
         return student.getIpn() >= 1000000000L && student.getIpn() <= 9999999999L;
     }
 
-    private boolean isGenderCorrect() {
+    public boolean isGenderCorrect() {
         if (student.getGender().equals("male")) {
             return (String.valueOf(student.getIpn()).charAt(8)) % 2 != 0;
         }
@@ -36,12 +37,34 @@ public class MyValidator implements ConstraintValidator<IpnValidator, Student> {
         }
     }
 
-    private boolean isDateOfBirthday() {
+    public boolean isDateOfBirthday() {
         long days = LocalDate.of(1899, 12, 31).until(student.getDateOfBirthday(), ChronoUnit.DAYS);
         StringBuilder daysFromIpn = new StringBuilder();
         daysFromIpn.append(String.valueOf(student.getIpn()).substring(0, 5));
-        logger.info("Days from ipn {}",daysFromIpn);
-        logger.info("Correct days {}",days);
+
+        logger.info("Days from ipn {}", daysFromIpn);
+        logger.info("Correct days {}", days);
+
         return days == Integer.parseInt(daysFromIpn.toString());
+    }
+
+    public boolean isControlDigit() {
+        LongStream longStream = String.valueOf(student.getIpn()).chars().mapToLong(Character::getNumericValue);
+        long[] arr = longStream.toArray();
+
+        long controlSum = arr[0] * (-1) + arr[1] * 5 + arr[2] * 7 + arr[3] * 9 + arr[4] * 4 + arr[5] * 6 + arr[6] * 10 +
+                arr[7] * 5 + arr[8] * 7;
+
+        long controlNumber = controlSum % 11;
+
+        logger.info("Control sum = {}, control number = {}", controlSum, controlNumber);
+
+        if (controlNumber > 9) {
+            controlNumber %= 10;
+        }
+
+        logger.info("Control number = {}", controlNumber);
+
+        return controlNumber == arr[arr.length - 1];
     }
 }
